625f1dd718e28c54e8844c00448aa4f820ad7e2f
marc-rios-tes <marc.rios@tes.tp.edu.tw>
2020-02-21 09:49:55 +0800

    Adding Hello World assignment.

---
+++ hello-world.py
@@ -0,0 +1,17 @@
+"""
+
+A Hello World test assignment.
+
+Create a function named `hello` which:
+ * Takes zero arguments.
+ * Prints "Hello, World!" to `STDOUT`. 
+ * Returns `None`. 
+
+Please keep in mind that all tests are case and space sensitive. So that means
+"hello world" or "hello, world!" or "Hello, world!" will all fail the test.
+Python has a brain, but it's not very smart. 
+
+"""
+
+
+# YOUR CODE GOES HERE!
